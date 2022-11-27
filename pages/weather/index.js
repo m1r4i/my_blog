29b3e4546925c35a9fb895beac9081f7783bed66
Mirai <m1r4i.dev@gmail.com>
2022-11-28 00:44:49 +0900

    Added: Weather page

--- pages/weather/index.js
+++ pages/weather/index.js
@@ -21,7 +21,7 @@ const getWeather =  async () => {
 
 export const getServerSideProps = async () => {
 
-  const weather = getWeather();
+  const weather = await getWeather();
 
   return {
     props: {
@@ -41,12 +41,12 @@ function Home({ weather }) {
     <div className="max-w-md py-4 px-8 bg-white shadow-lg rounded-lg my-20">
     <div>
       <h2 className="text-gray-800 text-3xl font-semibold">みずおかあおい居住地の天気</h2>
-      <p className="mt-2 text-gray-600">天気: {weather.weather[0].main}<br />{weather.weather[0].description}</p>
+      <p className="mt-2 text-gray-600">天気: {weather.weather[0].main} - {weather.weather[0].description}</p>
       <p className="mt-2 text-gray-600">気温: {weather.main.temp}℃</p>
       <p className="mt-2 text-gray-600">体感気温: {weather.main.feels_like}℃</p>
       <p className="mt-2 text-gray-600">気圧: {weather.main.pressure}hPa</p>
       <p className="mt-2 text-gray-600">湿度: {weather.main.humidity}%</p>
-
+      <p className="mt-2 text-gray-600">視程: {weather.visibility}m</p>
     </div>
   </div>
   )
