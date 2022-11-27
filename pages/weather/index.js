199a3b416d81ced8b7a3a4a71aa08133ab50f5c3
Mirai <m1r4i.dev@gmail.com>
2022-11-28 00:39:56 +0900

    Added: Weather page

--- pages/weather/index.js
+++ pages/weather/index.js
@@ -1,8 +1,7 @@
 //Loading Components and Styles
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import Post from '../components/Post';
+import styles from '../../styles/Home.module.css'
 
 
 //Loading modules
